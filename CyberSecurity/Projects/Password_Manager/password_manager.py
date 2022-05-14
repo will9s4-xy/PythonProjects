9697cb58e7dbef8d2b85f12ef004a3d285f3174b
William Sanefski <william2.sanefski@gmail.com>
2022-05-14 07:01:00 -0700

    first commit for password manager project

---
+++ CyberSecurity/Projects/Password_Manager/password_manager.py
@@ -0,0 +1,12 @@
+user = input("What is your name")
+password = input("Please fill out the password you want to save")
+
+key = input("We need to encrypt the file pick a number from 1 -26")
+sybmols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def view_information():
+    pass
+def add_information():
+    pass
+def encrypt_decrpt_information():
+    pass
